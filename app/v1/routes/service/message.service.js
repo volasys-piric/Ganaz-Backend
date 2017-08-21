38fe2ac8ea9539aa1b59ddbf58cb933a649fbc4d
Allan G. Ramirez <ramirezag@gmail.com>
2017-08-21 19:59:33 +0800

    Fixes https://bitbucket.org/volasys-ss/ganaz-backend/wiki/7.3%20Message%20-%20Create#markdown-header-change-log-v14

--- app/v1/routes/service/message.service.js
+++ app/v1/routes/service/message.service.js
@@ -1,10 +1,12 @@
 const Promise = require('bluebird');
 const db = require('./../../db');
+const twilioService = require('./twilio.service');
 const sendNotification = require('./../../../push_notification');
 const logger = require('./../../../utils/logger');
 
 const Message = db.models.message;
 const User = db.models.user;
+const Company = db.models.company;
 
 const find = function (body) {
   const $or = [];
@@ -31,18 +33,51 @@ const findById = function (id) {
 
 const create = function (body) {
   // TODO: Add validation such that job_id (if not null) and sender/receiver iuser_id and company_id (if not null) should be existing.
-  const receivers = body.receivers;
   const saveMessagePromises = [];
-  if (receivers.length > 0) {
-    for (let i = 0; i < receivers.length; i++) {
-      body.receiver = receivers[i];
+  if (body.receivers) {
+    const receivers = body.receivers;
+    if (receivers.length) {
+      for (let i = 0; i < receivers.length; i++) {
+        body.receiver = receivers[i];
+        const message = new Message(body);
+        saveMessagePromises.push(message.save());
+      }
+    } else {
       const message = new Message(body);
       saveMessagePromises.push(message.save());
     }
-  } else {
-    const message = new Message(body);
-    saveMessagePromises.push(message.save());
   }
+  const sendPushNotification = function (user, savedMessage) {
+    const jsonMessage = savedMessage.toObject();
+    let messageString = null;
+    if (typeof jsonMessage.message === 'object') {
+      messageString = jsonMessage.message.en;
+    } else {
+      // Assumed to be string
+      messageString = jsonMessage.message;
+    }
+    const messageId = jsonMessage._id.toString();
+    const data = {type: jsonMessage.type};
+    data.contents = {
+      id: messageId,
+      message_id: messageId,
+      message: messageString,
+    };
+    if (body.job_id) {
+      // For backward compatibility
+      data.contents.job_id = body.job_id
+    }
+    if (jsonMessage.type === 'application') {
+      data.contents.application_id = body.metadata.application_id;
+    } else if (jsonMessage.type === 'recruit') {
+      data.contents.recruit_id = body.metadata.recruit_id;
+    } else if (jsonMessage.type === 'suggest') {
+      data.contents.suggest_id = body.metadata.suggest_id;
+      data.contents.suggested_phone_number = body.metadata.suggested_phone_number;
+    }
+    sendNotification(user.player_ids, {contents: {en: messageString}, data: data});
+  };
+
   return Promise.all(saveMessagePromises).then(function (savedMessages) {
     for (let i = 0; i < savedMessages.length; i++) {
       // Send message to user/receiver ignoring result
@@ -51,34 +86,7 @@ const create = function (body) {
         User.findById(savedMessage.receiver.user_id).then(function (user) {
           if (user) {
             if (user.player_ids) {
-              const jsonMessage = savedMessage.toObject();
-              let messageString = null;
-              if (typeof jsonMessage.message === 'object') {
-                messageString = jsonMessage.message.en;
-              } else {
-                // Assumed to be string
-                messageString = jsonMessage.message;
-              }
-              const messageId = jsonMessage._id.toString();
-              const data = {type: jsonMessage.type};
-              data.contents = {
-                id: messageId,
-                message_id: messageId,
-                message: messageString,
-              };
-              if (body.job_id) {
-                // For backward compatibility
-                data.contents.job_id = body.job_id
-              }
-              if (jsonMessage.type === 'application') {
-                data.contents.application_id = body.metadata.application_id;
-              } else if (jsonMessage.type === 'recruit') {
-                data.contents.recruit_id = body.metadata.recruit_id;
-              } else if (jsonMessage.type === 'suggest') {
-                data.contents.suggest_id = body.metadata.suggest_id;
-                data.contents.suggested_phone_number = body.metadata.suggested_phone_number;
-              }
-              sendNotification(user.player_ids, {contents: {en: messageString}, data: data});
+              sendPushNotification(user, savedMessage);
             } else {
               logger.warn('[Message Service] Not sending push notification. User with id ' + savedMessage.receiver.user_id + ' has no player_ids.');
             }
@@ -91,7 +99,73 @@ const create = function (body) {
       }
     }
     return savedMessages;
-  })
+  }).then(function (savedMessages) {
+    // https://bitbucket.org/volasys-ss/ganaz-backend/wiki/7.3%20Message%20-%20Create#markdown-header-change-log-v14
+    if (body.receivers_phone_numbers) {
+      const findUserPromises = [];
+      for (let i = 0; i < body.receivers_phone_numbers.length; i++) {
+        const localNumber = body.receivers_phone_numbers[i];
+        findUserPromises.push(User.findOne({'phone_number.local_number': localNumber}));
+      }
+      return Promise.all(findUserPromises).then(function (users) {
+        const saveMessagePromises = [];
+        const validUsers = []; // Holder of user. We'll need this later
+        const noUserPhoneNumbers = [];
+        const existingReceiverUserIds = []; // Avoid duplicate creation of Message
+        for (let i = 0; i < savedMessages.length; i++) {
+          existingReceiverUserIds.push(savedMessages[i].receiver.user_id);
+        }
+
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i];
+          if (user !== null) {
+            if (existingReceiverUserIds.indexOf(user._id.toString()) === -1) { // Avoid duplicate creation of Message
+              validUsers.push(user);
+              body.receiver = {user_id: user._id.toString()};
+              if (user.company) {
+                body.receiver.company_id = user.company.company_id;
+              }
+              const message = new Message(body);
+              saveMessagePromises.push(message.save());
+            }
+          } else {
+            noUserPhoneNumbers.push(body.receivers_phone_numbers[i]);
+          }
+        }
+        return Promise.all(saveMessagePromises).then(function (additionalSavedMessages) {
+          for (let i = 0; i < additionalSavedMessages.length; i++) {
+            // Send message to user/receiver ignoring result
+            const savedMessage = additionalSavedMessages[i];
+            savedMessages.push(savedMessage);
+            const user = validUsers[i];
+            if (user.player_ids) {
+              sendPushNotification(user, savedMessage);
+            } else {
+              logger.warn('[Message Service] Not sending push notification. User with id ' + savedMessage.receiver.user_id + ' has no player_ids.');
+            }
+          }
+          if (noUserPhoneNumbers.length > 0) {
+            // Send SMS asynchronously and ignore result
+            Company.findById(body.sender.company_id).then(function (company) {
+              const companyName = company.name.en;
+              const body = companyName + ' quisiera recomendar que ud baje la aplicación Ganaz para poder recibir mensajes sobre el trabajo y tambien buscar otros trabajos en el futuro. http://www.GanazApp.com/download';
+              for (let i = 0; i < noUserPhoneNumbers.length; i++) {
+                const toFullNumber = "+1" + noUserPhoneNumbers[i];
+                twilioService.sendMessage(toFullNumber, body).catch(function (err) {
+                  logger.warn(err);
+                })
+              }
+            }).catch(function (err) {
+              logger.error(err);
+            });
+          }
+          return savedMessages;
+        });
+      });
+    } else {
+      return savedMessages;
+    }
+  });
 };
 
 const updateStatus = function (id, status) {
