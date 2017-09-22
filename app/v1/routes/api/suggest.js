dff89ca46006c4cfa701c0dfaa7a660405e9cb99
Allan G. Ramirez <ramirezag@gmail.com>
2017-09-22 09:18:45 +0800

    Set sender to body.worker_user_id if specified. Else, set sender to current user

--- app/v1/routes/api/suggest.js
+++ app/v1/routes/api/suggest.js
@@ -6,6 +6,7 @@ const httpUtil = require('./../../../utils/http');
 const messageService = require('./../service/message.service');
 
 const Suggest = db.models.suggest;
+const User = db.models.user;
 const Job = db.models.job;
 
 router.post('/', function (req, res) {
@@ -26,36 +27,36 @@ router.post('/', function (req, res) {
     if (job === null) {
       return Promise.reject('Job with id ' + body.job_id + ' does not exists.');
     } else {
-      if (!body.worker_user_id) {
-        body.worker_user_id = req.user._id;
-      }
-      const suggest = new Suggest(body);
-      return suggest.save().then(function (suggest) {
-        const senderId = req.user._id;
-        const senderCompanyId = req.user.company ? req.user.company.company_id : "";
-        const messageBody = {
-          job_id: body.job_id,
-          type: 'suggest',
-          sender: {
-            user_id: senderId,
-            company_id: senderCompanyId
-          },
-          receivers: [{
-            user_id: job.company_user_id // No need to pass company_id since messageService.create will retrieve it
-          }],
-          message: {
-            'en': 'New Job Inquiry for Referring a worker: ' + job.title.en,
-            'es': 'Nueva solicitud de empleo para referir a un trabajador: ' + (job.title.es ? job.title.es : job.title.en)
-          },
-          auto_translate: false,
-          datetime: Date.now(),
-          metadata: {
-            suggest_id: suggest._id.toString(),
-            suggested_phone_number: body.suggested_worker.phone_number.local_number
-          }
-        };
-        return messageService.create(messageBody).then(function () {
-          return suggest;
+      const getUserPromise = body.worker_user_id ? User.findById(body.worker_user_id) : Promise.resolve(req.user);
+      return getUserPromise.then(function (user) {
+        const userId = user._id.toString();
+        body.worker_user_id = userId;
+        const suggest = new Suggest(body);
+        return suggest.save().then(function (suggest) {
+          const messageBody = {
+            job_id: body.job_id,
+            type: 'suggest',
+            sender: {
+              user_id: body.worker_user_id,
+              company_id: user.company ? user.company.company_id : ""
+            },
+            receivers: [{
+              user_id: job.company_user_id // No need to pass company_id since messageService.create will retrieve it
+            }],
+            message: {
+              'en': 'New Job Inquiry for Referring a worker: ' + job.title.en,
+              'es': 'Nueva solicitud de empleo para referir a un trabajador: ' + (job.title.es ? job.title.es : job.title.en)
+            },
+            auto_translate: false,
+            datetime: Date.now(),
+            metadata: {
+              suggest_id: suggest._id.toString(),
+              suggested_phone_number: body.suggested_worker.phone_number.local_number
+            }
+          };
+          return messageService.create(messageBody).then(function () {
+            return suggest;
+          });
         });
       });
     }
