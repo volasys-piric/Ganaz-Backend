01266cfd06f743e1b1da95931883a8fad27e331f
Allan G. Ramirez <ramirezag@gmail.com>
2017-10-04 13:50:23 +0800

    Fixes conflicts between Survey and Smslog

--- app/v1/db/models/smslog.js
+++ app/v1/db/models/smslog.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TwilioResponseSchema = new Schema({}, {strict: false});
-const SurveySchema = new Schema({
+const SmslogSchema = new Schema({
   sender: {
     user_id: {$type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     company_id: {$type: mongoose.Schema.Types.ObjectId, ref: 'Company'}
@@ -22,11 +22,11 @@ const SurveySchema = new Schema({
   datetime: {$type: Date}
 }, {typeKey: '$type'});
 
-SurveySchema.pre('save', function (next) {
+SmslogSchema.pre('save', function (next) {
   if (!this.datetime) {
     this.datetime = Date.now();
   }
   next();
 });
 
-module.exports = mongoose.model('Survey', SurveySchema);
+module.exports = mongoose.model('Smslog', SmslogSchema);
