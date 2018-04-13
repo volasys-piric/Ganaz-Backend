c5a2b68e9be7c74f14a35ea6ff4690bb36c307c5
Allan G. Ramirez <ramirezag@gmail.com>
2018-04-13 20:44:20 +0800

    New structure of invite model

--- app/v1/db/models/invite.js
+++ app/v1/db/models/invite.js
@@ -8,14 +8,30 @@ const PhoneNumberSchema = new Schema({
 });
 
 const InviteSchema = new Schema({
-  user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  created_at: Date,
+  // ============== Deprecated since v1.12 =============== //
+  user_id: {$type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   company_id: String,
   phone_number: {
-    type: PhoneNumberSchema,
+    $type: PhoneNumberSchema,
     required: true
   },
-  created_at: Date
-});
+  // ============ Supported from v1.12 ================ //
+  sender: {
+    company_id: {$type: mongoose.Schema.Types.ObjectId, ref: 'Company'},
+    user_id: {$type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  },
+  receiver: {
+    type: {$type: String, enum: ['worker', 'company-group-leader']},
+    worker: {                                        // Optional
+      phone_number: {$type: PhoneNumberSchema}
+    },
+    company_group_leader: {                         // Optional
+      phone_number: {$type: PhoneNumberSchema},
+      crews: [{$type: mongoose.Schema.Types.ObjectId, ref: 'Crew'}]
+    }
+  },
+}, {typeKey: '$type'});
 
 InviteSchema.pre('save', function (next) {
   if (!this.created_at) {
